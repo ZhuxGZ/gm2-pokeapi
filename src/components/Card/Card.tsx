9269edf841a09f28e2a1a48f26ee17c20ed7b545
Ignacio Bergantiños <bergantinos.ignacio@gmail.com>
2023-09-26 10:15:10 -0300

    feat: Add fetch for the image and place the content center

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -1,10 +1,18 @@
+import { useEffect, useState } from 'react';
 import './Card.css';
 interface CardProps {
 	name: string;
-	image: string;
 }
 
-export const Card: React.FC<CardProps> = ({ name, image }) => {
+export const Card: React.FC<CardProps> = ({ name }) => {
+	const [image, setImage] = useState<string>();
+
+	useEffect(() => {
+		fetch(`https://pokeapi.co/api/v2/pokemon/${name}`)
+			.then((response) => response.json())
+			.then((data) => setImage(data.sprites.front_default));
+	}, [name]);
+
 	return (
 		<div className="card">
 			<img src={image} alt="" />

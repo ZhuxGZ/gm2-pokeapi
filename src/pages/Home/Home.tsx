e42ec807002bdf21013b055c7a4cdc6c61f40787
Ignacio Bergantiños <bergantinos.ignacio@gmail.com>
2023-09-28 12:50:47 -0300

    feat: Add the fetch and the increment and decrement function for the index

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,20 +1,47 @@
 import { Button, ButtonGroup, CardView, SearchBar } from '../../components';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+
+export interface IPokemon {
+	name: string;
+	url: string;
+}
 
 export const Home = () => {
-	const [filterBy, setFilterBy] = useState('');
 	const [index, setIndex] = useState(0);
+	const [pokemons, setPokemons] = useState<IPokemon[]>([]);
+	const [searchText, setSearchText] = useState<string>('');
+	const [pokemonsCount, setPokemonsCount] = useState<number>(0);
+
+	useEffect(() => {
+		fetch(`https://pokeapi.co/api/v2/pokemon?limit=10000&offset=0`)
+			.then((response) => response.json())
+			.then((data) => {
+				setPokemonsCount(data.count);
+				console.log(data.count);
+				setPokemons(data.results);
+			});
+	}, []);
+
+	const filteredPokemons = pokemons.filter((p) => p.name.includes(searchText));
+	const pageQuantity = Math.floor(pokemonsCount / 20); //20 is the number of cards per page
+
+	const incrementIndex = () => {
+		setIndex(index === pageQuantity ? pageQuantity : index + 1);
+	};
+	const decrementIndex = () => {
+		setIndex(index === 0 ? 0 : index - 1);
+	};
 
 	return (
 		<>
 			<div style={{ display: 'grid', placeContent: 'center' }}>
-				<SearchBar getFilter={setFilterBy} />
+				<SearchBar setSearchText={setSearchText} />
 			</div>
 			<ButtonGroup>
-				<Button handleClick={() => setIndex(index === 0 ? 0 : index - 1)}>Prev</Button>
-				<Button handleClick={() => setIndex(index + 1)}>Next</Button>
+				<Button handleClick={decrementIndex}>Prev</Button>
+				<Button handleClick={incrementIndex}>Next</Button>
 			</ButtonGroup>
-			<CardView filterBy={filterBy} index={index} />
+			<CardView pokemons={filteredPokemons} index={index} />
 		</>
 	);
 };

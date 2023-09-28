2ba04e767ad82e1f477e1b48948b1ece3e2d26e3
Ignacio Bergantiños <bergantinos.ignacio@gmail.com>
2023-09-28 11:15:43 -0300

    feat: Add PokemonTypes component

---
+++ src/components/PokemonTypes/PokemonTypes.tsx
@@ -0,0 +1,34 @@
+import './PokemonTypes.css';
+import { Badge } from '@chakra-ui/react';
+import { IType } from '../../assets/types/PokemonType';
+
+interface PokemonTypesProps {
+	types: IType[];
+}
+
+const TYPE_COLORS = {
+	normal: 'grey',
+	fire: 'red',
+	water: 'blue',
+	grass: 'green',
+	flying: 'blue',
+};
+
+export const PokemonTypes = ({ types }: PokemonTypesProps) => {
+	return (
+		<div className="pokemon-types">
+			{types.map((type) => {
+				return (
+					<Badge
+						key={type.type.name}
+						margin={'0.5em'}
+						fontSize={'1em'}
+						colorScheme={TYPE_COLORS[type.type.name as keyof typeof TYPE_COLORS]}
+					>
+						{type.type.name}
+					</Badge>
+				);
+			})}
+		</div>
+	);
+};
